ed6f938ae7d2e19bc2cdd511a29722c3489a662b
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-10-15 08:27:47 +0000

    uv環境ロック

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -17,7 +17,7 @@ fi
 # Activate the virtual environment and install dependencies
 source ./.venv/bin/activate
 uv sync
-uv add torch numpy pandas matplotlib jupyerlab
+uv add torch numpy pandas matplotlib jupyterlab
 
 # add astral-uv venv activation to bashrc
 echo 'source ./.venv/bin/activate' >> ~/.bashrc
