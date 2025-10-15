6bbc3fcd8c98bebfcc8e556b840da1b8ab8a2dcb
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-10-15 08:27:00 +0000

    GPU確認まで
    --- message ---
    
    Update Dockerfile and postCreateScript for improved environment setup and dependency management

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# # install astral-uv
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+# PATH="/home/ubuntu/.local/bin:$PATH"
+
+# Initialize repository as an astral-uv project if not already initialized
+if [ ! -f "./pyproject.toml" ]; then
+    uv init
+fi
+
 # Create an astral-uv virtual environment if the virtual environment does not exist
-if [ ! -d "/workspace/.venv" ]; then
+if [ ! -d "./.venv" ]; then
     uv venv --python 3.12
 fi
 
 # Activate the virtual environment and install dependencies
-source /workspace/.venv/bin/activate
+source ./.venv/bin/activate
 uv sync
-uv add pytorch
+uv add torch numpy pandas matplotlib jupyerlab
+
+# add astral-uv venv activation to bashrc
+echo 'source ./.venv/bin/activate' >> ~/.bashrc
+
+# check nvidia-smi
+echo $(nvidia-smi) > nvidia-smi.out.txt
